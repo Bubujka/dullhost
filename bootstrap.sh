322f426204d18e5b69207351a01bf85bf9745577
Aleksej Kamynin <zendzirou@gmail.com>
2014-09-07 18:07:56 +0400

    Исправления для новой убунты =_=

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo 'Acquire::http { Proxy "http://192.168.56.1:3142"; };' > /etc/apt/apt.conf.d/02proxy
+#echo 'Acquire::http { Proxy "http://192.168.56.1:3142"; };' > /etc/apt/apt.conf.d/02proxy
 cp /vagrant/sakura /usr/local/bin
 sakura init
